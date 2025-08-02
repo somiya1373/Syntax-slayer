9308116f718a5a6586b4f876e32b303496e4e60f
somiya1373 <157354242+somiya1373@users.noreply.github.com>
2025-08-02 14:36:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from manage_disasters import save_report, get_all_reports
 import os
 
 app = Flask(__name__)
-FILE = "credentials.txt"
+CRED_FILE = "credentials.txt"
+
+# ------------------------ Auth ----------------------------
 
 def load_users():
-    if not os.path.exists(FILE):
+    if not os.path.exists(CRED_FILE):
         return {}
-    with open(FILE, "r") as f:
+    with open(CRED_FILE, "r") as f:
         lines = f.read().splitlines()
     return {line.split(":")[0]: line.split(":")[1] for line in lines if ":" in line}
 
 def save_user(username, password):
-    with open(FILE, "a") as f:
+    with open(CRED_FILE, "a") as f:
         f.write(f"{username}:{password}\n")
 
 @app.route("/")
@@ -26,7 +29,7 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         if username in users and users[username] == password:
-            return "<h2 style='color:green;text-align:center;'>Login Successful!</h2>"
+            return redirect(url_for("report"))
         else:
             return "<h2 style='color:red;text-align:center;'>Invalid Credentials</h2>"
     return render_template("login.html")
@@ -46,6 +49,35 @@ def register():
 @app.route("/success")
 def success():
     return render_template("success.html")
+
+# ------------------- Disaster Reporting -------------------
+
+@app.route("/report")
+def report():
+    return render_template("index.html")
+
+@app.route("/submit-report", methods=["POST"])
+def submit_report():
+    try:
+        data = request.get_json()
+        result = save_report(
+            data.get("title"),
+            data.get("description"),
+            float(data.get("user_lat")),
+            float(data.get("user_lng")),
+            float(data.get("disaster_lat")),
+            float(data.get("disaster_lng")),
+        )
+        return jsonify(result)
+    except Exception as e:
+        print("Error occurred:", e)
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+@app.route("/get-disasters")
+def get_disasters():
+    return jsonify(get_all_reports())
+
+# --------------------- Start App --------------------------
 
 if __name__ == "__main__":
     app.run(debug=True)
